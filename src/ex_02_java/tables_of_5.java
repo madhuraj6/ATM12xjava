85726fc1172cf7628d7734f866fb1664075b20a5
madhuraj6 <madhukumardr9164@gmail.com>
2025-05-11 10:50:51 +0530

    ax

---
+++ src/ex_02_java/tables_of_5.java
@@ -0,0 +1,26 @@
+package ex_02_java;
+
+public class tables_of_5 {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.printf("5*1=   %d", num * 1);
+        System.out.println();
+        System.out.printf("5*2=  %d", num * 2);
+        System.out.println();
+        System.out.printf("5*3=  %d", num * 3);
+        System.out.println();
+        System.out.printf("5*4=  %d", num * 4);
+        System.out.println();
+        System.out.printf("5*5=  %d", num * 5);
+        System.out.println();
+        System.out.printf("5*6=  %d", num * 6);
+        System.out.println();
+        System.out.printf("5*7=  %d", num * 7);
+        System.out.println();
+        System.out.printf("5*8=  %d", num * 8);
+        System.out.println();
+        System.out.printf("5*9=  %d", num * 9);
+        System.out.println();
+        System.out.printf("5*10= %d", num * 10);
+    }
+}
