b460cf8ec2120f268cb4303056e6f90b17fec92a
madhuraj6 <madhukumardr9164@gmail.com>
2025-05-11 10:51:32 +0530

    axx

---
+++ src/ex_02_java/printf01.java
@@ -0,0 +1,27 @@
+package ex_02_java;
+
+public class printf01 {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.printf("5*1=   %d",num*1);
+        System.out.println();
+        System.out.printf("5*2=  %d",num*2);
+        System.out.println();
+        System.out.printf("5*3=  %d",num*3);
+        System.out.println();
+        System.out.printf("5*4=  %d",num*4);
+        System.out.println();
+        System.out.printf("5*5=  %d",num*5);
+        System.out.println();
+        System.out.printf("5*6=  %d",num*6);
+        System.out.println();
+        System.out.printf("5*7=  %d",num*7);
+        System.out.println();
+        System.out.printf("5*8=  %d",num*8);
+        System.out.println();
+        System.out.printf("5*9=  %d",num*9);
+        System.out.println();
+        System.out.printf("5*10= %d",num*10);
+
+    }
+}
